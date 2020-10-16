da0e4ada042f15804a21ebd2a108053152dc8a98
EmilyAlice2708 <emilyalicesesto@gmail.com>
2020-10-16 13:53:13 +0100

    Game self.create and self.instance methods created to avoid use of global variable! Web app now fully functional for a single game.

--- lib/app.rb
+++ lib/app.rb
@@ -11,24 +11,26 @@ class Battle < Sinatra::Base
     post '/names' do
         player_1 = Player.new(params[:Player_1_Name])
         player_2 = Player.new(params[:Player_2_Name])
-        $game = Game.new(player_1, player_2)
+        @game = Game.create(player_1, player_2)
         redirect to('/play')
     end 
 
     get '/play' do
-        @game = $game
+        @game = Game.instance
         erb(:play)
     end
 
+    before do
+        @game = Game.instance
+    end
+
     post '/attack_received' do
-        @game = $game
         @game.current_turn == @game.player_1 ? @game.attack(@game.player_2) : @game.attack(@game.player_1)        
         @game.switch_turn
         redirect '/attack'
     end
 
     get '/attack' do
-        @game = $game
         if @game.current_turn.points <= 0
             redirect '/game_over'
         end
@@ -36,7 +38,6 @@ class Battle < Sinatra::Base
     end
 
     get '/game_over' do
-        @game = $game
         @game.switch_turn
         erb(:game_over)
     end
