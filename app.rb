d4b44094eba8e4ab8e0e7feffd4cb620de257996
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-18 15:59:34 +0100

    Files rearranged.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require_relative 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
     enable :sessions
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,4 +1,4 @@
-require './lib/app.rb'
+require './app.rb'
 
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
